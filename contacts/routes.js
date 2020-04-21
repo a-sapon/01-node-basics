d0511adb3987b21c47262ac61608d0e7e9e22eb0
a-sapon <sapon.anya@gmail.com>
2020-04-21 21:10:15 +0300

    started work on 05

--- contacts/routes.js
+++ contacts/routes.js
@@ -8,6 +8,20 @@ const {
   validateInfoForUpdate,
   validateId
 } = require('./controllers');
+// const path = require('path');
+// const shortId = require('shortid');
+// const multer  = require('multer');
+
+// const storage = multer.diskStorage({
+//   destination: 'draft',
+//   filename: function (req, file, cb) {
+//     const filename = shortId();
+//     const ext = path.parse(file.originalname).ext;
+//     cb(null, filename + ext);
+//   }
+// })
+
+// const upload = multer({storage});
 
 const router = Router();
 
@@ -16,5 +30,11 @@ router.get('/:contactId', validateId, getContactById);
 router.post('', addContact);
 router.delete('/:contactId', validateId, removeContact);
 router.patch('/:contactId', validateId, validateInfoForUpdate, updateContact);
+
+// router.post('/upload', upload.single('file_example'), minifyImg, (req, res) => {
+//   console.log('req.file', req.file);
+
+//   res.status(200).send();
+// })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -24,6 +24,7 @@ module.exports = class UserServer {
   initMiddlewares() {
     this.server.use(cors());
     this.server.use(express.json());
+    this.server.use(express.static('uploads'));
   }
 
   initRoutes() {
